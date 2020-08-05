b8537eb63316b352d8456867186086b487ab5955
Anh Nhân Nguyễn <anhnhan@outlook.com>
2020-08-06 01:28:26 +0200

    lint ormconfig.ts

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,25 +1,25 @@
 
 // import _ from 'lodash';
 
-var _ =  require('lodash');
+const _ =  require('lodash');
 
 
 module.exports = _.extend({
-  "type": "mysql",
-  "synchronize": false,
-  "logging": false,
-  "entities": [
-    "src/entity/**/*.ts"
+  type: 'mysql',
+  synchronize: false,
+  logging: false,
+  entities: [
+    'src/entity/**/*.ts'
   ],
-  "migrations": [
-    "src/migration/**/*.ts"
+  migrations: [
+    'src/migration/**/*.ts'
   ],
-  "subscribers": [
-    "src/subscriber/**/*.ts"
+  subscribers: [
+    'src/subscriber/**/*.ts'
   ],
-  "cli": {
-    "entitiesDir": "src/entity",
-    "migrationsDir": "src/migration",
-    "subscribersDir": "src/subscriber"
+  cli: {
+    entitiesDir: 'src/entity',
+    migrationsDir: 'src/migration',
+    subscribersDir: 'src/subscriber'
   }
 }, require('./config.database.json'));
