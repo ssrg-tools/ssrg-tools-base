3594cf5b33f9ab0e5e9f2c602882dad637c203c8
Anh Nhân Nguyễn <anhnhan@outlook.com>
2020-08-11 01:47:43 +0200

    api: add PaginationResult

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,21 @@
 
-export
-interface BaseApiResponse<T> {
+export interface BaseApiResponse<T> {
   data: T;
 
-  time_taken: string;
+  timeTakenMs: string;
+}
+
+export class PaginationResult<T> implements BaseApiResponse<T[]> {
+  data: T[];
+  total: number;
+  pageSize: number;
+
+  timeTakenMs: string;
+
+  constructor([data, total]: [T[], number], pageSize: number)
+  {
+    this.data = data;
+    this.total = total;
+    this.pageSize = pageSize;
+  }
 }
