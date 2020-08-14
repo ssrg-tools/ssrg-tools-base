d1d89e3f23beebe3c79d09ebae0be6e8bee5ca17
Anh Nhân Nguyễn <anhnhan@outlook.com>
2020-08-14 22:59:51 +0200

    api: iterate pagination

--- src/api.ts
+++ src/api.ts
@@ -8,14 +8,16 @@ export interface BaseApiResponse<T> {
 export class PaginationResult<T> implements BaseApiResponse<T[]> {
   data: T[];
   total: number;
+  page: number;
   pageSize: number;
 
   timeTakenMs: string;
 
-  constructor([data, total]: [T[], number], pageSize: number)
+  constructor([data, total]: [T[], number], pageSize: number, page = 0)
   {
     this.data = data;
     this.total = total;
+    this.page = page;
     this.pageSize = pageSize;
   }
 }
