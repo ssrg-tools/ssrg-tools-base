df29bdf222639311aaa3c011a4a7d87d1eb8b8dd
Anh Nhân Nguyễn <anhnhan@outlook.com>
2020-08-23 01:19:31 +0200

    api/pagination: allow to pass date(-string)s as page

--- src/api.ts
+++ src/api.ts
@@ -9,12 +9,12 @@ export interface BaseApiResponse<T> {
 export class PaginationResult<T> implements BaseApiResponse<T[]> {
   data: T[];
   total: number;
-  page: number;
+  page: number | string;
   pageSize: number;
 
   timeTakenMs: string;
 
-  constructor([data, total]: [T[], number], pageSize: NumberLike, page = 0)
+  constructor([data, total]: [T[], number], pageSize: NumberLike, page: number | string = 0)
   {
     this.data = data;
     this.total = total;
