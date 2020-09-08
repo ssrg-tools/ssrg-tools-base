818c23879e85f24aaf2448df637296c2625b4fd4
Anh Nhân Nguyễn <anhnhan@outlook.com>
2020-09-08 23:23:35 +0200

    clear cards: add utility methods

--- src/entity/SongClearCard.ts
+++ src/entity/SongClearCard.ts
@@ -82,4 +82,76 @@ export class SongClearCard {
   })
   @JoinColumn([{ name: 'theme_id', referencedColumnName: 'id' }])
   theme: Theme;
+
+  get canIncreaseGrade()
+  {
+    switch (this.grade) {
+      case 'R':
+        return false;
+      case 'S':
+      case 'A':
+      case 'B':
+      case 'C':
+      default:
+        return true;
+    }
+  }
+
+  get canDecreaseGrade()
+  {
+    switch (this.grade) {
+      default:
+      case 'C':
+        return false;
+      case 'R':
+      case 'S':
+      case 'A':
+      case 'B':
+        return true;
+    }
+  }
+
+  decreaseGrade()
+  {
+    switch (this.grade) {
+      case 'R':
+        this.grade = 'S';
+        break;
+      case 'S':
+        this.grade = 'A';
+        break;
+      case 'A':
+        this.grade = 'B';
+        break;
+      case 'B':
+        this.grade = 'C';
+        break;
+      case 'C':
+      default:
+        break;
+    }
+  }
+
+  increaseGrade()
+  {
+    switch (this.grade) {
+      case 'R':
+        break;
+      case 'S':
+        this.grade = 'R';
+        break;
+      case 'A':
+        this.grade = 'S';
+        break;
+      case 'B':
+        this.grade = 'A';
+        break;
+      case 'C':
+        this.grade = 'B';
+        break;
+      default:
+        this.grade = 'C';
+        break;
+    }
+  }
 }
