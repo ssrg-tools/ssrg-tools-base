2f7e39e0ddf206b9763e0d10471d8027faf2b53b
Anh Nhân Nguyễn <anhnhan@outlook.com>
2020-09-26 00:15:51 +0200

    score-calculator: start calculating theme grade bonus for R deck

--- src/score-calculator.ts
+++ src/score-calculator.ts
@@ -8,5 +8,12 @@ export function calculateIdealScore(cards: Card[], themeBonus: number, cardbookB
 
 export function calculateThemeBonus(cards: Card[])
 {
-  return 545000;
+  if (!cards.length) {
+    return 0;
+  }
+
+  const firstTheme = cards[0].theme;
+  if (cards.every(card => card.grade === 'R' && _.isEqual(firstTheme, card.theme))) {
+    return 500000 + Math.min(0, (cards.length - 3) * 15000);
+  }
 }
