4ffbeb8e7c8778a67d13fed04fee52968670cef3
Anh Nhân Nguyễn <anhnhan@outlook.com>
2020-09-27 17:26:05 +0200

    externalize divGroupIcons

---
+++ src/league-tracker.ts
@@ -0,0 +1,11 @@
+
+export const divGroupIcons = {
+  gfriend: { 1: '🍎', 2: '🍏' },
+  starship: { 1: '☄️', 2: '🌌' },
+  jyp: { 1: 'Apricot', 2: 'Pink' },
+
+  woollim: { 1: 'A', 2: 'B' },
+  sm: { 1: 'A', 2: 'B' },
+  pledis: { 1: 'A', 2: 'B' },
+  test: { 1: 'A', 2: 'B' },
+};
