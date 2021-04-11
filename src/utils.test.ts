bb67428bbc33fa0a2f0d2b12f820615a4ffa4658
Anh Nhân Nguyễn <anhnhan@outlook.com>
2021-04-11 13:48:53 +0200

    utils: add scoreBonusInDateRange and scoreBonusCountdown

---
+++ src/utils.test.ts
@@ -0,0 +1,44 @@
+
+import 'jasmine';
+import { scoreBonusCountdown, scoreBonusInDateRange } from './utils';
+
+const scoreBonusDateData: [Date, Date, boolean][] = [
+  [ new Date('2020-06-10T11:00:00Z'), new Date('2020-06-06T00:00:00Z'), false ],
+  [ new Date('2020-06-10T11:00:00Z'), new Date('2020-06-06T14:00:00Z'), false ],
+  [ new Date('2020-06-10T11:00:00Z'), new Date('2020-06-06T16:00:00+02:00'), false ],
+  [ new Date('2020-06-10T11:00:00Z'), new Date('2020-06-06T16:59:00+02:00'), false ], // not yet in KST
+  [ new Date('2020-06-10T11:00:00Z'), new Date('2020-06-06T16:00:00Z'), true ], // yes in KST
+  [ new Date('2020-06-10T11:00:00Z'), new Date('2020-06-07T00:00:00Z'), true ],
+  [ new Date('2020-06-10T11:00:00Z'), new Date('2020-06-07T11:00:00Z'), true ],
+  [ new Date('2020-06-10T11:00:00Z'), new Date('2020-06-08T11:00:00Z'), true ],
+  [ new Date('2020-06-10T11:00:00Z'), new Date('2020-06-09T11:00:00Z'), true ],
+  [ new Date('2020-06-10T11:00:00Z'), new Date('2020-06-10T11:00:00Z'), true ],
+  [ new Date('2020-06-10T11:00:00Z'), new Date('2020-06-11T11:00:00Z'), true ],
+  [ new Date('2020-06-10T11:00:00Z'), new Date('2020-06-12T11:00:00Z'), true ],
+  [ new Date('2020-06-10T11:00:00Z'), new Date('2020-06-13T11:00:00Z'), true ],
+  [ new Date('2020-06-10T11:00:00Z'), new Date('2020-06-13T16:59:00+02:00'), true ],
+  [ new Date('2020-06-10T11:00:00Z'), new Date('2020-06-13T17:00:00+02:00'), false ],
+
+  [ new Date('2020-12-31T11:00:00Z'), new Date('2021-01-01T11:00:00Z'), true ],
+];
+
+describe('Score bonus date test', () => {
+  scoreBonusDateData.forEach(([ datePoint, dateNow, expected ]) => {
+    it(`Score bonus date for ${dateNow} within ${datePoint} should be ${expected}`, () => {
+      expect(scoreBonusInDateRange(datePoint, dateNow)).toBe(expected);
+    });
+  });
+});
+
+const scoreBonusCountdownData: [Date, Date, number][] = [
+  [ new Date('2020-06-10T11:00:00Z'), new Date('2020-06-06T00:00:00Z'), 1 ],
+  [ new Date('2020-11-31T11:00:00Z'), new Date('2021-01-01T11:00:00Z'), 334 ],
+];
+
+describe('Score bonus countdown test', () => {
+  scoreBonusCountdownData.forEach(([ datePoint, dateNow, expected ]) => {
+    it(`Score bonus date for ${dateNow} within ${datePoint} should be ${expected}`, () => {
+      expect(scoreBonusCountdown(datePoint, dateNow)).toBe(expected);
+    });
+  });
+});
--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,38 @@
+import moment from 'moment-timezone';
+
 export function boxSplit(input: string | string[], split = ' ') {
   if (typeof input === 'string') {
     return input.split(split);
   }
   return input;
+}
+
+export function scoreBonusInDateRange(datePoint: Date, dateNow: Date = new Date()) {
+  const scoreBonusDayDiff = 3;
+  const mDatePoint = moment(datePoint).tz('Asia/Seoul');
+  const mDatePointStart = mDatePoint.clone().add(-scoreBonusDayDiff, 'day').startOf('day');
+  const mDatePointEnd = mDatePoint.clone().add(scoreBonusDayDiff, 'day').endOf('day');
+
+  const mDateNow = moment(dateNow).tz('Asia/Seoul');
+
+  // if (current >= start && current <= end) {
+  if (mDateNow >= mDatePointStart && mDateNow <= mDatePointEnd) {
+    return true;
+  }
+
+  return false;
+}
+
+export function scoreBonusCountdown(datePoint: Date, dateNow: Date = new Date()) {
+  const scoreBonusDayDiff = 3;
+  const mDateNow = moment(dateNow).tz('Asia/Seoul').startOf('day');
+  const mDatePoint = moment(datePoint).tz('Asia/Seoul').year(mDateNow.year());
+  const mDatePointStart = mDatePoint;
+  const mDatePointEnd = mDatePoint.clone().add(scoreBonusDayDiff, 'day').endOf('day');
+  if (mDateNow > mDatePointEnd) {
+    mDatePointStart.year(mDateNow.year() + 1);
+  }
+  mDatePointStart.add(-scoreBonusDayDiff, 'day').startOf('day');
+
+  return mDatePointStart.diff(mDateNow, 'days');
 }
