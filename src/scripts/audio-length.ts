c8447adf4ad0fa97e70de2cfaf983ea3661bc4e2
Anh Nhân Nguyễn <anhnhan@outlook.com>
2021-04-25 01:28:38 +0200

    package: add get-audio-duration

---
+++ src/scripts/audio-length.ts
@@ -0,0 +1,12 @@
+import { getAudioDurationInSeconds } from 'get-audio-duration';
+import { resolve } from 'path';
+
+const path = process.argv[2];
+if (!path) {
+  console.error('No path provided.');
+  process.exit(1);
+}
+
+getAudioDurationInSeconds(path).then((duration) => {
+  console.log(resolve(path), duration);
+});
