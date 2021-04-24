b4b91786d95e45e81a21e35afc12832a161d07ed
Anh Nhân Nguyễn <anhnhan@outlook.com>
2021-04-24 03:26:45 +0200

    constants: add gnpSignature

--- src/constants.ts
+++ src/constants.ts
@@ -8,3 +8,5 @@ export const cardSizeLarge = {
   width: 226,
   height: 318,
 };
+
+export const gnpSignature = Buffer.from(['\x89', '\x50', '\x4e', '\x47', '\x0d', '\x0a', '\x1a', '\x0a'].reverse().join(''));
