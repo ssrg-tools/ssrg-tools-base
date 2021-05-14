e6b1c7b3c4fa306159d4ed0d4e6c55f47be44fc7
Anh Nhân Nguyễn <anhnhan@outlook.com>
2021-05-14 20:48:21 +0200

    entity-helpers/EncryptedVarchar.embed: change to use buffers for compatibility

--- src/entity-helpers/EncryptedVarchar.embed.ts
+++ src/entity-helpers/EncryptedVarchar.embed.ts
@@ -3,21 +3,23 @@ import _sodium from 'libsodium-wrappers';
 
 export class EncryptedVarchar {
   @Column('tinyblob')
-  encryptedValue: Uint8Array;
+  encryptedValue: Buffer;
 
   @Column('tinyblob')
-  nonce: Uint8Array;
+  nonce: Buffer;
 
   saveValue(sodium: typeof _sodium, key: Uint8Array, value: string) {
-    this.nonce = sodium.randombytes_buf(sodium.crypto_secretbox_NONCEBYTES);
-    this.encryptedValue = sodium.crypto_secretbox_easy(value, this.nonce, key);
+    this.nonce = Buffer.from(
+      sodium.randombytes_buf(sodium.crypto_secretbox_NONCEBYTES),
+    );
+    this.encryptedValue = Buffer.from(
+      sodium.crypto_secretbox_easy(value, this.nonce, key),
+    );
   }
 
-  getValue(sodium: typeof _sodium, key: Uint8Array): Uint8Array {
-    return sodium.crypto_secretbox_open_easy(
-      this.encryptedValue,
-      this.nonce,
-      key,
+  getValue(sodium: typeof _sodium, key: Uint8Array): Buffer {
+    return Buffer.from(
+      sodium.crypto_secretbox_open_easy(this.encryptedValue, this.nonce, key),
     );
   }
 }
