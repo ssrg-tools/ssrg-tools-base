10b81d987533b6d32225faa59b1a77cbc166a03a
Anh Nhân Nguyễn <anhnhan@outlook.com>
2022-01-21 03:31:23 +0100

    package: better exports, better build and publish settings

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+// Entities are not export in root.
+export * from './api';
+export * from './backend-interface';
+export * from './constants';
+export * from './dalcom';
+export * from './dalcom-files';
+export * from './discord';
+export * from './gamedata';
+export * from './guid';
+export * from './league-tracker';
+export * from './rp';
+export * from './score-calculator';
+export * from './seq';
+export * from './types';
+export * from './utils';
+export * from './wr';
